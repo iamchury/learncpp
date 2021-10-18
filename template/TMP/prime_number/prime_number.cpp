bb7b9f58d91a883f345d8071bfbe1dc7263f74da
Chury Jeong <iamchury@gmail.com>
2021-10-18 20:37:31 +0900

    running code, no compile error.
    typename은 result_type이 class 타입이라는 것을 컴파일러에게 명확하게 알려주기 위해서 필요함.
    default는 값이다.
    is_prime<11> 이 실행되는 순서는,
    is_prime<11> -> _is_prime<INT<11>>::result ->
    _is_prime<<INT<11>> -> check_div<"11",two>::result
    ->
    "11"::num % two::num ||
    check_div<"11", typename add<two,one>
    
    "11"::num % three::num ||
    check_div<"11",typename add<three,one>
    
    "11"::num % "4"::num ||
    check_div<"11",typename add<"4",one>
    
    *** 11/2 = 5 이다. 따라서 specialization이 실행된다.
    check_div<"11", "5"> 는, 아래 코드가 이용된다.
    결과는 return false이다.
    struct check_div<N, typename divide<N, two>::result_type> {
    	static const bool result = (N::num % (N::num / 2) == 0);
    };
    
    모든 코드를 펼쳐보면,
    (11%2 == 0) || (11%3 == 0 ) || (11%4 == 0) || (11%5 == 0)  이 된다.
    모두 false이므로 false가되고,
    _is_prime = !check_div<N,two>::result 로, 반대값을 가지므로, _is_prime은 true가 된다.
    정말 대단한 코드이다.

--- template/TMP/prime_number/prime_number.cpp
+++ template/TMP/prime_number/prime_number.cpp
@@ -25,27 +25,58 @@ int main()
 	std::cout << "Is prime ? :: " << is_prime(61) << std::endl; // true
 }
 #else
+
+template <int N>
+struct INT {
+	static const int num = N;
+};
+
+template <typename a, typename b>
+struct add {
+	typedef INT<a::num + b::num> result_type; // add의 result는 타입이다. 
+};
+
+template <typename a, typename b>
+struct divide {
+	typedef INT<a::num / b::num> result_type; // divide의 result는 타입이다. 
+};
+
+using one = INT<1>;
+using two = INT<2>;
+using three = INT<3>;
+
+template <typename N, typename d> 
+struct check_div {
+	//result 중에서 한개라도 true이면 전체가 true
+	// check_div의 result는 bool 타입의 값이다. 
+	static const bool result = (N::num % d::num == 0) ||
+		check_div<N, typename add<d, one>::result_type>::result;
+
+	// typename add<d,one>::result 에서 result는 특정값이 아니라, class type이라는 것을 compile에게 
+	// 명확하게 지시하기 위해서 typename 을 붙여준다. typename 이 없으면 값으로 해석한다. 
+};
+
+template <typename N>
+struct _is_prime {
+	static const bool result = !check_div<N, two>::result;
+};
 template <>
-struct is_prime<2> {
+struct _is_prime<two> {
 	static const bool result = true;
 };
 template <>
-struct is_prime<3> {
+struct _is_prime<three> {
 	static const bool result = true;
+};
+
+template <typename N>
+struct check_div<N, typename divide<N, two>::result_type> {
+	static const bool result = (N::num % (N::num / 2) == 0);
 };
 
 template <int N>
 struct is_prime {
-	static const bool result = !check_div<N, 2>::result;
-};
-
-template <int N, int d> 
-struct check_div {
-	static const bool result = (N % d == 0) || check_div<N, d + 1>::result;
-};
-template <int N> 
-struct check_div<N, N / 2> {
-	struct const bool result = (N % (N / 2) == 0);
+	static const bool result = _is_prime<INT<N>>::result;
 };
 
 int main()
@@ -55,5 +86,8 @@ int main()
 	std::cout << "Is prime ? :: " << is_prime<10>::result << std::endl; // false
 	std::cout << "Is prime ? :: " << is_prime<11>::result << std::endl; // true
 	std::cout << "Is prime ? :: " << is_prime<61>::result << std::endl; // true
+
+	std::cout << "type one: " << one::num << std::endl;
+	std::cout << "add class: " << add<one,three>::result_type::num << std::endl;
 }
 #endif
