7f4af0bba86034a33adc2b1925cda5227b7cc85c
Chury Jeong <iamchury@gmail.com>
2021-10-25 16:19:33 +0900

    external comparator used.

--- stl/set/set.cpp
+++ stl/set/set.cpp
@@ -99,11 +99,64 @@ void set_ex3()
 	print_set_range(todos);
 
 }
+
+#include <string>
+
+template <typename T, typename C>
+void print_set(std::set<T, C>& s) {
+	// 셋의 모든 원소들을 출력하기
+	for (const auto& elem : s) {
+		std::cout << elem << " " << std::endl;
+	}
+}
+class Todo4 {
+	int priority;
+	std::string job_desc;
+
+public:
+	Todo4(int priority, std::string job_desc)
+		: priority(priority), job_desc(job_desc) {}
+
+	friend struct TodoCmp;
+
+	friend std::ostream& operator<<(std::ostream& o, const Todo4& td);
+};
+struct TodoCmp {
+	bool operator()(const Todo4& t1, const Todo4& t2) const {
+		if (t1.priority == t2.priority) {
+			return t1.job_desc < t2.job_desc;
+		}
+		return t1.priority > t2.priority;
+	}
+};
+
+std::ostream& operator<<(std::ostream& o, const Todo4& td) {
+	o << "[ 중요도: " << td.priority << "] " << td.job_desc;
+	return o;
+}
+void set_ex4() {
+	std::set<Todo4, TodoCmp> todos;
+
+	todos.insert(Todo4(1, "농구 하기"));
+	todos.insert(Todo4(2, "수학 숙제 하기"));
+	todos.insert(Todo4(1, "프로그래밍 프로젝트"));
+	todos.insert(Todo4(3, "친구 만나기"));
+	todos.insert(Todo4(2, "영화 보기"));
+
+	print_set(todos);
+
+	std::cout << "-------------" << std::endl;
+	std::cout << "숙제를 끝냈다면!" << std::endl;
+	todos.erase(todos.find(Todo4(2, "수학 숙제 하기")));
+	print_set(todos);
+}
+
 int main()
 {
 	set_ex1();
 	set_ex2();
 	set_ex3();
+	set_ex4();
 
 	return 0;
 }
