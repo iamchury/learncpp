86be0ffccd15f3a84e00335c794d496be9185832
Chury Jeong <iamchury@gmail.com>
2021-11-04 23:00:06 +0900

    remove_if example code

--- stl/algo_remove/algo_remove.cpp
+++ stl/algo_remove/algo_remove.cpp
@@ -12,7 +12,7 @@ void print(Iter begin, Iter end) {
     }
     std::cout << std::endl;
 }
-int main() {
+void remove_ex() {
     std::vector<int> vec;
     std::vector<int>::iterator itr;
     vec.push_back(5);
@@ -36,6 +36,33 @@ int main() {
     vec.erase(itr, vec.end());
     std::cout << "Vector size : " << vec.size() << std::endl;
     print(vec.begin(), vec.end());
+}
+
+struct is_odd {
+    bool operator()(const int& i) { return i % 2 == 1; }
+};
+
+void remove_if_ex() {
+    std::vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+
+    std::cout << "처음 vec 상태 ------" << std::endl;
+    print(vec.begin(), vec.end());
+
+    std::cout << "벡터에서 홀수 인 원소 제거 ---" << std::endl;
+    vec.erase(std::remove_if(vec.begin(), vec.end(), is_odd()), vec.end());
+    print(vec.begin(), vec.end());
+}
+
+int main()
+{
+    remove_ex();
+    remove_if_ex();
 }
 
 
