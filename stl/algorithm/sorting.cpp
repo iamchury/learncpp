27f8386e00cb41f29005e0fff8185eb052f7e9af
Chury Jeong <iamchury@gmail.com>
2021-11-04 21:06:20 +0900

    internal functor template greater example code.

--- stl/algorithm/sorting.cpp
+++ stl/algorithm/sorting.cpp
@@ -49,8 +49,26 @@ void sort_op_ex() {
     print(vec.begin(), vec.end());
 }
 
+void sort_greater_ex() {
+    std::vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(6);
+    vec.push_back(4);
+    vec.push_back(7);
+    vec.push_back(2);
+
+    std::cout << "정렬 전 ----" << std::endl;
+    print(vec.begin(), vec.end());
+    std::sort(vec.begin(), vec.end(), std::greater<int>());
+
+    std::cout << "Greater Reverse 정렬 후 ----" << std::endl;
+    print(vec.begin(), vec.end());
+}
 int main()
 {
     sort_ex();
     sort_op_ex();
+    sort_greater_ex();
 }
