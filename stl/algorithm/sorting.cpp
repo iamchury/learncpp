fb7a596450aee1b6ccf71f05c53b07b74e59dccb
Chury Jeong <iamchury@gmail.com>
2021-11-04 21:02:34 +0900

    sort in descending

--- stl/algorithm/sorting.cpp
+++ stl/algorithm/sorting.cpp
@@ -10,7 +10,7 @@ void print(Iter begin, Iter end) {
     }
     std::cout << std::endl;
 }
-int main() {
+void sort_ex() {
     std::vector<int> vec;
     vec.push_back(5);
     vec.push_back(3);
@@ -26,4 +26,31 @@ int main() {
 
     std::cout << "정렬 후 ----" << std::endl;
     print(vec.begin(), vec.end());
+}
+
+struct int_compare {
+    bool operator()(const int& a, const int& b) const { return a > b; }
+};
+void sort_op_ex() {
+    std::vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(6);
+    vec.push_back(4);
+    vec.push_back(7);
+    vec.push_back(2);
+
+    std::cout << "정렬 전 ----" << std::endl;
+    print(vec.begin(), vec.end());
+    std::sort(vec.begin(), vec.end(),int_compare());
+
+    std::cout << "Reverse 정렬 후 ----" << std::endl;
+    print(vec.begin(), vec.end());
+}
+
+int main()
+{
+    sort_ex();
+    sort_op_ex();
 }
